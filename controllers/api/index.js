ae08664b4891d203015529cb2321932df6a4d11a
Cassieellaelaine <129123349+Cassieellaelaine@users.noreply.github.com>
2023-07-27 00:09:14 -0400

    Add cool face API

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const poetryRoutes = require('./poetryRoutes');
+const cool = require('cool-ascii-faces')
 
 router.use('/users', userRoutes);
 router.use('/poetry', poetryRoutes);
+router.use('/cool', cool)
 
 module.exports = router;
